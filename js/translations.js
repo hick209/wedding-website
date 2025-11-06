3ec65db50cf782ffc062c1dd88f636f42ff8e5d6
Nivaldo Bondança <nbondanca@meta.com>
2025-11-06 09:46:33 -0500

    Language change without reload

--- js/translations.js
+++ js/translations.js
@@ -31,25 +31,23 @@ function applyTranslations(lang) {
     }
     element.textContent = translations[lang][key];
   });
+
+  const newUrl = new URL(window.location.href);
+  newUrl.searchParams.set('lang', lang);
+  window.history.pushState(null, '', newUrl.toString());
+}
+
+function getCurentLanguage() {
+  return new URLSearchParams(window.location.search).get('lang') || 'en';
 }
 
 // Initial load with default language
 document.addEventListener('DOMContentLoaded', () => {
-  const selectedLang = new URLSearchParams(window.location.search).get('lang') || 'en';
-  console.log(`selectedLang: ${selectedLang}`);
-  const langToggle = document.getElementById('lang-toggle');
-  const nextLang = selectedLang === 'en' ? 'pt' : 'en';
-  console.log(`nextLang: ${nextLang}`);
-  langToggle.href = `?lang=${nextLang}`;
-  langToggle.addEventListener('click', () => {
-    const newUrl = new URL(window.location.href).searchParams.set('lang', nextLang);
-    console.log("newUrl:");
-    console.log(newUrl.toString());
-    console.log("newUrl.search:");
-    console.log(newUrl.search.toString());
-    window.location.search = newUrl.search;
-    window.location.href = newUrl.toString();
+  document.getElementById('lang-toggle').addEventListener('click', () => {
+    const nextLang = getCurentLanguage() === 'en' ? 'pt' : 'en';
+    applyTranslations(nextLang);
   });
+
   // Apply translations for initial load
-  applyTranslations(selectedLang);
+  applyTranslations(getCurentLanguage());
 });
