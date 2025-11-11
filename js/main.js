9d95c70d66581e08827b31032f53b7b3c968fb13
Nivaldo Bondança <nbondanca@meta.com>
2025-11-11 10:45:22 -0500

    CSS clean up

--- js/main.js
+++ js/main.js
@@ -22,29 +22,6 @@
     var clone2 = $('.menu-2 > ul').clone();
     $('#fh5co-offcanvas').append(clone2);
 
-    $('#fh5co-offcanvas .has-dropdown').addClass('offcanvas-has-dropdown');
-    $('#fh5co-offcanvas')
-        .find('li')
-        .removeClass('has-dropdown');
-
-    // Hover dropdown menu on mobile
-    $('.offcanvas-has-dropdown')
-        .mouseenter(function(){
-          var $this = $(this);
-          $this
-              .addClass('active')
-              .find('ul')
-              .slideDown(500, 'easeOutExpo');
-        })
-        .mouseleave(function(){
-          var $this = $(this);
-          $this
-              .removeClass('active')
-              .find('ul')
-              .slideUp(500, 'easeOutExpo');
-        });
-
-
     $(window).resize(function(){
       if ( $('body').hasClass('offcanvas') ) {
         $('body').removeClass('offcanvas');
@@ -108,29 +85,6 @@
       } , { offset: '85%' } );
   };
 
-
-  var dropdown = function() {
-
-      $('.has-dropdown').mouseenter(function(){
-
-          var $this = $(this);
-          $this
-              .find('.dropdown')
-              .css('display', 'block')
-              .addClass('animated-fast fadeInUpMenu');
-
-      }).mouseleave(function(){
-          var $this = $(this);
-
-          $this
-              .find('.dropdown')
-              .css('display', 'none')
-              .removeClass('animated-fast fadeInUpMenu');
-      });
-
-  };
-
-
   var testimonialCarousel = function(){
       var owl = $('.owl-carousel-fullwidth');
       owl.owlCarousel({
@@ -178,25 +132,6 @@
     $(".fh5co-loader").fadeOut("slow");
   };
 
-  var counter = function() {
-    $('.js-counter').countTo({
-      formatter: function (value, options) {
-        return value.toFixed(options.decimals);
-      },
-    });
-  };
-
-  var counterWayPoint = function() {
-    if ($('#fh5co-counter').length > 0 ) {
-      $('#fh5co-counter').waypoint( function( direction ) {
-        if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-          setTimeout( counter , 400);
-          $(this.element).addClass('animated');
-        }
-      } , { offset: '90%' } );
-    }
-  };
-
   // Parallax
   var parallax = function() {
     $(window).stellar({
@@ -212,12 +147,9 @@
     offcanvasMenu();
     burgerMenu();
     contentWayPoint();
-    dropdown();
     testimonialCarousel();
     goToTop();
     loaderPage();
-    counter();
-    counterWayPoint();
   });
 
 }());
